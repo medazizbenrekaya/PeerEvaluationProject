a2c17cdb550550ce72a51f29c237096aa5e2d96f
azizbenrekaya <azizbenrekaya@gmail.com>
2020-03-26 19:25:16 +0100

    Team , MicroSkills Back

---
+++ Pidev/api/microskills.js
@@ -0,0 +1,77 @@
+var express = require('express')
+var router = express.Router()
+var user = require('../models/user')
+var ms = require('../models/microskills')
+
+
+
+
+
+
+
+router.post("/ajouterMS", (req, res) => {
+    var micro = new ms(req.body);
+    micro.save((err, c) => {
+        if (err) res.json(err);
+        else res.json(c);
+    });
+});
+
+router.post("/accepter/:id", (req, res) => {
+    var x = true
+
+    user.findOne({email: req.body.email}, (err, u) => {
+        ms.findOne({_id: req.params.id}, (err, c) => {
+
+            u.microskills.forEach(function (ee) {
+                if(ee.id==c.id){
+                    x=false;
+
+                }
+
+            })
+
+
+            if(x==false)
+                res.status(401).json({info :"Vous l'avez déja"})
+
+            else{
+                u.microskills.push(c)
+                console.log(c)
+                u.save(function (err) {
+                    if (err)
+                        console.log('error')
+                    else
+                        res.json('success')
+                });
+            }});
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ Pidev/api/team.js
@@ -0,0 +1,64 @@
+var express = require('express')
+var router = express.Router()
+var Team = require('../models/team');
+var user = require('../models/user')
+
+
+
+
+router.post("/ajouter", (req, res) => {
+    var team = new Team(req.body);
+    team.save((err, c) => {
+        if (err) res.json(err);
+        else res.json(c);
+    });
+});
+
+
+
+router.post("/accepter/:id", (req, res) => {
+    var x = true
+
+    user.findOne({email: req.body.email}, (err, u) => {
+        Team.findOne({_id: req.params.id}, (err, c) => {
+
+            c.members.forEach(function (ee) {
+                if(ee.id==u.id){
+                    x=false;
+
+                }
+
+            })
+
+
+            if(x==false)
+                res.status(401).json({info :"tu est deja dans la team"})
+
+            else{
+                c.members.push(u)
+                console.log(c)
+                c.save(function (err) {
+                    if (err)
+                        console.log('error')
+                    else
+                        res.json('success')
+                });
+            }});
+    });
+})
+router.get("/getmembers/:id", (req, res) => {
+    var x = true
+
+
+        Team.findOne({_id: req.params.id}, (err, c) => {
+
+          if(c)
+              res.json(c.members)
+            else
+                res.status(401).json('Team Introuvable')
+        });
+
+})
+
+
+module.exports = router;

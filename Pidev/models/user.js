266af19cbd5509df3505bbd729b7c5066d64f017
azizbenrekaya <azizbenrekaya@gmail.com>
2020-03-30 16:59:37 +0100

    test

--- Pidev/models/user.js
+++ Pidev/models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 var Team = require('../models/team');
-var MS = require('./microskills').schema
+var MS = require('./microSkills').schema
 var team = require('../models/team')
 var userSchema = new mongoose.Schema({
   nom : {type : String , trim : true},

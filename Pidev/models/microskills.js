7408cd261ded6e86b0a36d773e6ac9458a1a4b86
azizbenrekaya <azizbenrekaya@gmail.com>
2020-03-30 16:50:33 +0100

    Note & Eval

--- Pidev/models/microskills.js
+++ Pidev/models/microskills.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose')
+var eval = require('./evaluation').schema
 var MicroSkillsSchema = new mongoose.Schema({
     nom : {type : String , trim : true , required : true},
     description : {type:String , required : true},
     macroskills : [{
         nom: {type: String, required: true},
         description: {type: String, required: true},
-        note:{type:Number}
+        notes:[eval]
     }
     ]
 })

b647c59f22c88baf3b8737b96302f1e8dc803e88
mohamedboufaied <mohamed.boufaied@esprit.tn>
2020-03-31 13:44:55 +0100

    commit

--- Pidev/models/microskills.js
+++ Pidev/models/microskills.js
@@ -2,9 +2,10 @@ var mongoose = require('mongoose')
 var MicroSkillsSchema = new mongoose.Schema({
     nom : {type : String , trim : true , required : true},
     description : {type:String , required : true},
-    macroSkills : [{
+    macroskills : [{
         nom: {type: String, required: true},
-        description: {type: String, required: true}
+        description: {type: String, required: true},
+        note:{type:Number,default:null}
     }
     ]
 })

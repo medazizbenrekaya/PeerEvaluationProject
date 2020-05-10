c09f92361b17e32e33813d0bc5aa3aa19250bfb1
Adem24 <adem.benzarb@esprit.tn>
2020-05-10 15:31:43 +0100

    test workshop

--- Pidev/api/workshop.js
+++ Pidev/api/workshop.js
@@ -3,6 +3,7 @@ var router = express.Router()
 var workshop = require('../models/workshop');
 var user = require('../models/user');
 var nodemailer = require("nodemailer");
+//var moment=require('moment')
 
 router.post("/ajouter", (req, res) => {
     workshop.findOne({nom: req.body.nom}).then(work => {
@@ -19,51 +20,46 @@ router.post("/ajouter", (req, res) => {
 })
 
 router.post("/reserver", (req, res) => {
-    workshop.findOne({_id: req.body.idws}).then(ws => {
-             user.findOne({_id: req.body.id}).then(t => {
-                 // ws.etudiants.forEach(e=>{
-                 //     if(e._id===t._id){
-                 //         res.json("déjà inscrit")
-                 //     }
-                 //     else
-                        if(ws.nbplace===0){
-                         res.json("il n'y a pas des places")
-                     }
-                     else if(ws.datefin<Date.now()){
-                         res.json("terminer")
-                     }
-                     else  {
-                         ws.etudiants.push(t)
-                         ws.nbplace--
-                         ws.save()
+    workshop.findOne({_id: req.body.idws}).then( ws => {
+        user.findOne({_id:req.body.ide}).then(t=> {
+            ws.etudiants.forEach(e => {
+                if (e == req.body.ide) {
+                    res.json("alrady exist")
+                } else if (ws.nbplace === 0) {
+                    res.json("il n'y a pas des places")
+                } else if (Date.parse(ws.datefin.toString())<Date.now()) {
+                    res.json("terminer")
+                } else {
+                    ws.etudiants.push(t)
+                    ws.nbplace--
+                    ws.save()
 
-                                    var smtpTransport = nodemailer.createTransport({
-                                        service: 'Gmail',
-                                        auth: {
-                                            user: 'benzarb34@gmail.com',
-                                            pass: '50502450'
-                                        }
-                                    });
-                                    var mailOptions = {
-                                        to: t.email,
-                                        from: 'peer@gmail.com',
-                                        subject:'reservation made',
-                                        text: '\n' +
-                                            'your reservation has been accepted for'+ws.nom+'\n\n'+
-                                            'will begin in :'+ws.datedebut+'\n\n'+
-                                            'will finish in :'+ws.datefin+'\n\n'
-                                    };
-                                    smtpTransport.sendMail(mailOptions, function(err) {
-                                        console.log('mail sent');
+                    var smtpTransport = nodemailer.createTransport({
+                        service: 'Gmail',
+                        auth: {
+                            user: 'benzarb34@gmail.com',
+                            pass: '50502450'
+                        }
+                    });
+                    var mailOptions = {
+                        to: t.email,
+                        from: 'peer@gmail.com',
+                        subject: 'reservation made',
+                        text: '\n' +
+                            'your reservation has been accepted for' + ws.nom + '\n\n' +
+                            'will begin in :' + ws.datedebut + '\n\n' +
+                            'will finish in :' + ws.datefin + '\n\n'
+                    };
+                    smtpTransport.sendMail(mailOptions, function (err) {
+                        console.log('mail sent');
 
-                                    });
+                    });
 
-                         res.json("réservation effectuer")
-                     }
-                 // })
-
-                })
-    })
+                    res.json("réservation effectuer")
+                }
+            })
+        })
+        })
 });
 
 router.post('/delete', function(req, res, next) {
